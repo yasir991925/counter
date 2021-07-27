6762d219fed4c4e1daab38ed63b8f870afac4193
Yasir Jafar <yasir991925@gmail.com>
2021-07-27 19:40:17 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [val, setVal] = useState(0);
+  let x = "value ";
+  if (val === 0) {
+    x = "value red";
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <h1 className={x}>{val}</h1>
+      </div>
+      <div className="controls">
+        <button onClick={() => setVal(val + 1)}>+1</button>
+        <button onClick={() => setVal(Math.max(0, val - 1))}>-1</button>
+        <button onClick={() => setVal(0)}>Reset</button>
+      </div>
     </div>
   );
 }
