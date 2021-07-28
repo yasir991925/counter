f480e0ddae70aa4cae79754737baaf504dcbcdaf
Yasir Jafar <yasir991925@gmail.com>
2021-07-29 01:24:06 +0530

    color changed resize fixed

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import "./App.css";
 
 function App() {
   const [val, setVal] = useState(0);
-  let x = "value ";
-  if (val === 0) {
-    x = "value red";
-  }
 
   return (
     <div className="App">
       <div className="main">
-        <h1 className={x}>{val}</h1>
-      </div>
-      <div className="controls">
-        <button onClick={() => setVal(val + 1)}>+1</button>
-        <button onClick={() => setVal(Math.max(0, val - 1))}>-1</button>
-        <button onClick={() => setVal(0)}>Reset</button>
+        <h1 className="value">{val}</h1>
+        <div className="controls">
+          <button className="hover-green" onClick={() => setVal(val + 1)}>
+            +1
+          </button>
+          <button className="hover-red" onClick={() => setVal(Math.max(0, val - 1))}>
+            -1
+          </button>
+          <button onClick={() => setVal(0)}>Reset</button>
+        </div>
       </div>
     </div>
   );
